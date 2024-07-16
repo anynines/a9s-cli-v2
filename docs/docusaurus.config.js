0e91a23408e1a6027413251f0badf9a79d8d84c9
Julian Fischer <ich@julianfischer.name>
2024-07-16 09:54:34 +0200

    Upgrade to docusaurus 3.0.0

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 const Versions = require("./versions.json");
 const usercentricsScript = ({ NODE_ENV }) => {
@@ -225,8 +226,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} anynines`
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme
+        theme: lightTheme,
+        darkTheme: darkTheme
       }
     })
 };
