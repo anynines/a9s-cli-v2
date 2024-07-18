13944ee469516a9f6c83dedef40dfa9c059394ec
Julian Fischer <ich@julianfischer.name>
2024-07-18 09:26:05 +0200

    Auto selection of k8s provider if non was specified

--- demo/required_commands.go
+++ demo/required_commands.go
@@ -12,9 +12,6 @@ func RequiredCommands() map[string]map[string]string {
 	cmds["docker"] = make(map[string]string)
 	cmds["docker"]["darwin"] = "brew install docker"
 
-	cmds["minikube"] = make(map[string]string)
-	cmds["minikube"]["darwin"] = "brew install minikube"
-
 	cmds["cmctl"] = make(map[string]string)
 	cmds["cmctl"]["darwin"] = "brew install cmctl"
 
